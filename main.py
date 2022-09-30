b9acbeeb2c028b04c1ec5bbf6a6719f1cdc7939a
ноуть <ilumarik5@gmail.com>
2022-09-30 19:59:57 +0300

    Задача

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-22626
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-u
+a = input("Число 1:")
+a = int(a)
+b = input("Число 2:")
+b = int(b)
+print(a + b)
+print(a - b)
+print(a * b)
