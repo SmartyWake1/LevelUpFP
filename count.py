f841f40f8800bf1eeb398b73b9a47bffeecfcdc3
ноуть <ilumarik5@gmail.com>
2022-10-04 22:16:12 +0300

    Задачи

---
+++ count.py
@@ -0,0 +1,22 @@
+# a = input()
+# print(a.count(' '))
+
+
+
+# n = int(input())
+# sum_strok = ""
+# for i in range(n):
+   # sum_strok += input()
+# print(sum_strok.count(" "))
+# print(sum_strok)
+
+
+
+
+
+# n = int(input())
+# summa_strok = ""
+# for i in range(n):
+  #  summa_strok += input()
+  #  summa_strok += " "
+# print(f"{summa_strok}")
---
+++ строки.py
@@ -0,0 +1,25 @@
+# a = "dfdsfdsf"
+# print(list(a))
+
+
+# username = "Tom"
+# userage = 23
+# user = f"My name is {username} my age is {userage}"
+# print(user)
+
+# my_str = "adaffa"
+# my_list = [1, 2, 3]
+# x = "a" in my_str
+# print(x)
+# part_list = my_list[1:3]
+# print(part_list)
+
+
+# my_str = "hello.pipi.dodo"
+# print(my_str.split())
+
+# text = input()
+# print(text.split())
+
+a = input()
+print(a.count(' '))
