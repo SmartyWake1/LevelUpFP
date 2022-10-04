d518c8eb2ebf78addf9a7705b7a47869a9d93205
ноуть <ilumarik5@gmail.com>
2022-10-04 19:55:56 +0300

    Задача со списком 1

---
+++ ЗадачаСписок.py
@@ -0,0 +1,10 @@
+
+# на вход программы поступает n чисел. Выведите их сумму
+n = int(input())
+my_list = []
+for i in range(n):
+    number = int(input())
+    my_list.append(number)
+print(my_list)
+
+# на вход программы поступает n чисел. Выведите их сумму. пример ввода "5 6 7"
