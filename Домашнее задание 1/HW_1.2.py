afcba9ea821e7de778dfea949f428a0f53178c88
ноуть <ilumarik5@gmail.com>
2022-10-05 19:05:18 +0300

    HW1.2

--- Домашнее задание 1/HW_1.2.py
+++ Домашнее задание 1/HW_1.2.py
@@ -1,10 +1,10 @@
 month_number = int(input("Введите номер месяца: "))
-month_list = [3, 5, 7, 8, 10, 12]
+month_list_31_days = [3, 5, 7, 8, 10, 12]
 if 0 >= month_number or month_number > 12:
     print("Такого номера месяца не существует!")
 elif month_number == 2:
     print("Февраль")
-elif month_number in month_list:
+elif month_number in month_list_31_days:
     print("31 день в месяце")
 else:
     print("30 дней в месяце")
