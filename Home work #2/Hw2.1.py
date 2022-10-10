199fc9aa8afbf21dfd85a92cd62dc42b19b714f1
ноуть <ilumarik5@gmail.com>
2022-10-10 03:02:37 +0300

    Home work 2.1

--- Home work #2/Hw2.1.py
+++ Home work #2/Hw2.1.py
@@ -1,10 +1,6 @@
 """
 Написать программу, считывающую список чисел и выводящую список в обратном порядке.
 4 5 6 2 1 6 8 34 2 1
-for i in range():
-    lst[0 + i]
-    c.append()
-
 """
 
 a = input("Список чисел: ").split()
@@ -14,7 +10,6 @@ for i in range(len(a)):
 print("Введеный список: ", lst)
 
 c = []
-i = 0
 for i in range(-1, -(len(lst) + 1), -1):
     c.append(int(lst[i]))
 
