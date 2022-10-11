5d24b53f7cb89ceaf0e41048e2ce8d81792dc5ff
ноуть <ilumarik5@gmail.com>
2022-10-11 19:58:25 +0300

    Задача 2

---
+++ Занятие5/Задание 2.py
@@ -0,0 +1,8 @@
+horizontal = 1
+for i in range(1, 11):
+    vertical = 1
+    for i in range(1, 11):
+        print(horizontal * vertical, end=" ")
+        vertical += 1
+    print(" ")
+    horizontal += 1
