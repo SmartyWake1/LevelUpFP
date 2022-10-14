d5d2586fa9c02a69f755bd0f3ee9443bc2795364
ноуть <ilumarik5@gmail.com>
2022-10-14 20:11:43 +0300

    Задача1

--- Занятие 6/Задача1.py
+++ Занятие 6/Задача1.py
@@ -1,3 +1,4 @@
+
 def list_main(lst):
     if sorted(lst) == lst:
         return True
@@ -6,11 +7,11 @@ def list_main(lst):
     else:
         return False
 
+if __name__ == "__main__":
+    a = input("Список чисел: ").split()
+    lst = []
+    for i in range(len(a)):
+        lst.append(int(a[i]))
 
-a = input("Список чисел: ").split()
-lst = []
-for i in range(len(a)):
-    lst.append(int(a[i]))
-
-a = list_main(lst)
-print(a)
+    a = list_main(lst)
+    print(a)
