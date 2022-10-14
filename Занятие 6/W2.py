e18ee4e26d4b9e046bc0a81eed1bb7848cd8a225
ноуть <ilumarik5@gmail.com>
2022-10-14 20:48:20 +0300

    Задача2

--- Занятие 6/W2.py
+++ Занятие 6/W2.py
@@ -1,19 +1,19 @@
-def countRange(lst, a_min, b_max):
+def countRange(lst, lst_min, lst_max):
     list = []
     for i in range(0, len(lst)):
-        if b_max < lst[0 + i] < a_min:
+        if lst_max < lst[0 + i] < lst_min:
             continue
-        elif b_max > lst[0 + i] > a_min:
+        elif lst_max > lst[0 + i] > lst_min:
             list.append(float(lst[0 + i]))
     return list
 
-if __name__ == "__main__":
-    a = input("Список чисел: ").split()
-    lst = []
-    for i in range(len(a)):
-        lst.append((float(a[i])))
-    lst_max = int(input("Максимальное значение: "))
-    lst_min = int(input("Минимальное значение: "))
 
-    c = countRange(lst, lst_min, lst_max)
-    print(c)
+a = input("Список чисел: ").split()
+lst = []
+for i in range(len(a)):
+    lst.append((float(a[i])))
+lst_max = int(input("Максимальное значение: "))
+lst_min = int(input("Минимальное значение: "))
+
+c = countRange(lst, lst_min, lst_max)
+print(c)
