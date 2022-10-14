057049c0b8fa43d9c9862a10dced44a8e6fa82da
ноуть <ilumarik5@gmail.com>
2022-10-14 22:49:59 +0300

    Занятие 6

--- Занятие 6/WorkDIct2.py
+++ Занятие 6/WorkDIct2.py
@@ -1,3 +1,8 @@
 My_dict1 = [1, 2, 3, 4]
 My_dict2 = [1, 4, 9, 16]
+My_dict3 = {}
 print(dict(zip(My_dict1, My_dict2)))
+
+for i in range(0, len(My_dict1)):
+    My_dict3[My_dict1[i]] = My_dict2[i]
+print(My_dict3)
