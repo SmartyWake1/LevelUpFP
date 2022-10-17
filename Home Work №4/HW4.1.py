c41995ed34b3d7285409b0d10e6926e37bf6f761
ноуть <ilumarik5@gmail.com>
2022-10-17 19:39:51 +0300

    HW4.1

--- Home Work №4/HW4.1.py
+++ Home Work №4/HW4.1.py
@@ -1,14 +1,15 @@
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
+a = input("3 числа: ").split()
 
-disc = b ** 2 - 4 * a * c
-print(disc)
+disc = int(a[1]) ** 2 - 4 * int(a[0]) * int(a[2])
+print("Диск", disc)
+
 if disc == 0:
-    print(f"x = {-b / 2 * a}")
+    print(f"x = {-int(a[1]) / 2 * int(a[0])}")
+
 elif disc < 0:
     print("Нет корней")
+
 else:
-    x1 = (-b + disc ** 0.5) / (2 * a)
-    x2 = (-b - disc ** 0.5) / (2 * a)
+    x1 = (-int(a[1]) + disc ** 0.5) / (2 * int(a[0]))
+    x2 = (-int(a[1]) - disc ** 0.5) / (2 * int(a[0]))
     print(f"x1 = {x1} \nx2 = {x2}")
