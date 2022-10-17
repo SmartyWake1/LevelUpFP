e128b7d9164de2211d5fbe0f17cffedaf8a6a5d1
ноуть <ilumarik5@gmail.com>
2022-10-17 03:43:24 +0300

    HW4.1

--- Home Work №4/HW4.1.py
+++ Home Work №4/HW4.1.py
@@ -5,10 +5,10 @@ c = float(input("c = "))
 disc = b ** 2 - 4 * a * c
 print(disc)
 if disc == 0:
-    print(-b / 2 * a)
+    print(f"x = {-b / 2 * a}")
 elif disc < 0:
     print("Нет корней")
 else:
     x1 = (-b + disc ** 0.5) / (2 * a)
     x2 = (-b - disc ** 0.5) / (2 * a)
-    print(x1, x2)
+    print(f"x1 = {x1} \nx2 = {x2}")
