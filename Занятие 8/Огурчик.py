4a74487714a2e8bbfdeb48204993f2f374245116
ноуть <ilumarik5@gmail.com>
2022-10-21 21:42:43 +0300

    pickle and json learning

--- Занятие 8/Огурчик.py
+++ Занятие 8/Огурчик.py
@@ -12,11 +12,11 @@ with open('OgyrezFile.pickle', "br") as Ogyrez_file:
     My = pickle.load(Ogyrez_file)
     print(My)
 score = 0
-if My[1] in "1":
+if My[1] == "1":
     score += 1
-if My[2] in "2":
+if My[2] == "2":
     score += 1
-if My[3] in "3":
+if My[3] == "3":
     score += 1
 print(score)
 
@@ -31,10 +31,10 @@ with open('NosokFile.json') as f:
     print(My1)
 
 score1 = 0
-if My1["1"] in "1":
+if My1["1"] != "1":
     score1 += 1
-if My1["2"] in "2":
+if My1["2"] != "2":
     score1 += 1
-if My1["3"] in "3":
+if My1["3"] != "3":
     score1 += 1
 print(score1)
