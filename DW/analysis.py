c09622e0a9a8abc1def9ecfe8d002305f1ffe6cd
ноуть <ilumarik5@gmail.com>
2022-10-26 17:42:56 +0300

    DW

--- DW/analysis.py
+++ DW/analysis.py
@@ -1,10 +1,13 @@
 import json
 
 def analysis():
-    with open("ParserRusult.json", "r", encoding="utf8") as File_analysis:
+    with open("ParserRusultURL.json", "r", encoding="utf8") as File_analysis:
         a = json.load(File_analysis)
         print(a)
-
+        for key in a:
+            print(a[key])
+            for Price in a[key]:
+                print(a[key][Price])
 
 if __name__ == "__main__":
     analysis()
